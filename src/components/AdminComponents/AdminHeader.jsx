0033a56498b3c6c754720a4d9e9606f78f6d9601
Henry Agu <aguhenrychuks@gmail.com>
2023-06-27 07:02:20 +0100

    fix: made little changes to PR

--- src/components/AdminComponents/AdminHeader.jsx
+++ src/components/AdminComponents/AdminHeader.jsx
@@ -1,30 +1,36 @@
-import { useContext } from 'react'
-import {NavLink} from 'react-router-dom'
-import menu from './icons/menu4.svg'
-import xIcon from './icons/x.svg'
-import './styles/AdminHeader.css'
-import {MyContext} from '../../Context'
-
-
+import { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import menu from "./icons/menu4.svg";
+import xIcon from "./icons/x.svg";
+import "./styles/AdminHeader.css";
+import { MyContext } from "../../Context";
 
 const AdminHeader = () => {
-    const contextValue = useContext(MyContext);
+  const contextValue = useContext(MyContext);
 
-    const {sharedState, toggleShareBoolean} = contextValue
-    
-    return(
-        <main>
-            <div>
-                <div className="header">
-                    <div className='heading'>
-                    <img className={sharedState ? 'clicked' : 'icon'} src={sharedState ? xIcon : menu} width={35} height={35} onClick={toggleShareBoolean}/>
-                        <h2 className="header1"> Uniport | Admin </h2> 
-                    </div>
-                    <NavLink to="/"><h2 className="header1 log">Logout</h2></NavLink>
-                </div>
-            </div>
-        </main>
-    )
-}
+  const { sharedState, toggleShareBoolean } = contextValue;
 
-export default AdminHeader
+  return (
+    <main>
+      <div>
+        <div className="header">
+          <div className="heading">
+            <img
+              className={sharedState ? "clicked" : "icon"}
+              src={sharedState ? xIcon : menu}
+              width={32}
+              height={32}
+              onClick={toggleShareBoolean}
+            />
+            <h2 className="header1"> Uniport | Admin </h2>
+          </div>
+          <NavLink to="/">
+            <h2 className="header1 log">Logout</h2>
+          </NavLink>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default AdminHeader;
